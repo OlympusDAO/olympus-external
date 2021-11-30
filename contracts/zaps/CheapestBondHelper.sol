58cb20770e96615764b26f1ddfe2656994fa6866
FinagleLord <72840971+FinagleLord@users.noreply.github.com>
2021-11-29 19:09:23 -0800

    Create CheapestBondHelper.sol
    finds cheapest bond depo while disregarding principle, so users can always zap into the cheapest bond

---
+++ contracts/zaps/CheapestBondHelper.sol
@@ -0,0 +1,55 @@
+pragma solidity 0.7.5;
+
+interface IDepository {
+    function bondPriceInUSD() external view returns (uint price_);
+    function principle() external view returns (address);
+}
+
+contract CheapestBondHelper {
+
+    //////////////// State ////////////////
+
+    IDepository[] public depos;
+
+    address public OlympusDAO;
+
+    //////////////// Init ////////////////
+
+    constructor (IDepository[] memory _depos) {
+        depos = _depos;
+
+        OlympusDAO = msg.sender;
+    }
+
+
+    ///////////// Policy Only /////////////
+
+    function update_Depos(IDepository[] memory _depos) external {
+        require(msg.sender == OlympusDAO);
+        depos = _depos;
+    }
+
+    function pushOwnership(address who) external {
+        require(msg.sender == OlympusDAO);
+        OlympusDAO = who;
+    }
+
+    //////////////// Public ////////////////
+
+    function getCheapestBond() external view returns (IDepository cheapestDepo, address principle) {
+
+        uint256 cheapestPrice = type(uint256).max;
+
+        for (uint256 i; i < depos.length; i++) {
+
+            uint256 price = depos[i].bondPriceInUSD();
+            
+            if (price <= cheapestPrice) {
+                cheapestPrice = price;
+                cheapestDepo = depos[i];
+            }
+        }
+
+        principle = cheapestDepo.principle();
+    }
+}
